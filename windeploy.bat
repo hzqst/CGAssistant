bce42ab59272214a5913dffd03bd9c2a9df073f1
hzqst <113660872@qq.com>
2021-03-21 10:17:49 +0800

    nodejs

--- windeploy.bat
+++ windeploy.bat
@@ -1,2 +1,11 @@
 cd build
+
 windeployqt CGAssistant.exe
+
+for /f "usebackq tokens=*" %%i in (`where node`) do (
+  set NodePath=%%i
+)
+
+if exist "%NodePath%" (
+    copy "%NodePath%" "node.exe" /y
+)
