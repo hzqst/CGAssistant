63f0437a14e0c73cef96363103256b87a7ff5243
hzqst <113660872@qq.com>
2021-05-26 19:48:57 +0800

    修复cga.gui.LoadAccount无法及时刷新登录账号的问题

--- cgahook/gameservice.cpp
+++ cgahook/gameservice.cpp
@@ -2269,6 +2269,10 @@ int CGAService::NewUI_SelectCharacter(int index, int a2)
 			if (UI_IsCharacterPresent(index))
 			{
 				m_fakeCGSharedMem[0] = 0;
+				m_ui_auto_login = false;
+				m_ui_selectbigserver_click_index = -1;
+				m_ui_selectserver_click_index = -1;
+				m_ui_selectcharacter_click_index = -1;
 				return 1;
 			}
 			else
@@ -2277,6 +2281,10 @@ int CGAService::NewUI_SelectCharacter(int index, int a2)
 				if (m_ui_create_character)
 				{
 					m_fakeCGSharedMem[0] = 0;
+					m_ui_auto_login = false;
+					m_ui_selectbigserver_click_index = -1;
+					m_ui_selectserver_click_index = -1;
+					m_ui_selectcharacter_click_index = -1;
 					return 0;
 				}
 				else
