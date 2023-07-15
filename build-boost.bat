9876c8b86fe61cee5c6bf28d1f2b4c2d03f6b238
hzqst <113660872@qq.com>
2023-07-15 12:34:50 +0800

    1

--- build-boost.bat
+++ build-boost.bat
@@ -8,3 +8,5 @@ call bootstrap.bat
 
 b2 --toolset=msvc-14.1 --with-date_time --with-thread --with-container --with-system --with-locale --with-serialization --with-regex --stagedir="stage" link=static stage
 b2 --toolset=msvc-14.1 --with-date_time --with-thread --with-container --with-system --with-locale --with-serialization --with-regex --stagedir="stage" runtime-link=static link=static stage
+
+pause
